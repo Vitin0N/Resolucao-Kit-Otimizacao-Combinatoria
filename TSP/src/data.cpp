7eed04276c553ae05bc52cfcf15558ff0f7e1cdc
Vitin0N <victormrdv2@gmail.com>
2026-08-23 21:48:39 -0300

    feat: modificando codigo do data.cpp para ele conseguir ler outros tipo de EWF

--- TSP/src/data.cpp
+++ TSP/src/data.cpp
@@ -38,38 +38,71 @@ void lerInstancias(const std::string& caminhoArquivo){
         }
     }
 
-    // Redimensiona a matriz global(ignorando o indice 0)
-    matrizDistancias.assign(dimensao + 1, std::vector<double>(dimensao + 1, 0.0));
-    matrizDimensao = dimensao;
+    // Verifica o formato de leitura do arquivo
+    std::string ewfTipo;
 
-    // Busca a seção das coordenadas
-    while(arquivo >> palavra){
-        if(palavra == "NODE_COORD_SECTION:" || palavra ==  "NODE_COORD_SECTION"){
+    // Busca o tipo de leitura
+    while(std::getline(arquivo, linha)){
+        if(linha.find("EDGE_WEIGHT_TYPE") != std::string::npos){
+            std::stringstream ss(linha);
+
+            ss >> palavra;
+            
+            // Limpa os dois pontos caso exista
+            if(!palavra.empty() && palavra.back() == ':'){
+                palavra.pop_back();
+            }
+
+            // Percorre o resto da linha procurando o tipo da linha
+            while(ss >> palavra){
+                if(palavra == ":") continue;
+
+                ewfTipo = palavra;
+            }
+
             break;
         }
     }
 
-    // Le as coordenadas
-    std::vector<double> coordX(dimensao + 1);
-    std::vector<double> coordY(dimensao + 1);
-    int id;
+    // Redimensiona a matriz global(ignorando o indice 0)
+    matrizDistancias.assign(dimensao + 1, std::vector<double>(dimensao + 1, 0.0));
+    matrizDimensao = dimensao;
+    
+    // Verifica o tipo do edge weigth e aloca os dados na matriz conforme
+    
+    if(ewfTipo == "EUC_2D"){
+        // Le as coordenadas
+        std::vector<double> coordX(dimensao + 1);
+        std::vector<double> coordY(dimensao + 1);
+        int id;
 
-    // Preenche os vetores das coordenadas
-    for(int i = 1; i <=  dimensao; i++){
-        arquivo >> id >> coordX[i] >> coordY[i];
-    }
-
-    // Preenchendo a matriz com as distâncias euclidianas
-    for(int i = 1; i <= dimensao; i++){
-        for(int j = 1; j <= dimensao; j++){
-            if(i != j){
-                double deltaX = coordX[i] - coordX[j];
-                double deltaY = coordY[i] - coordY[j];
-
-                matrizDistancias[i][j] = std::round(std::sqrt(deltaX * deltaX + deltaY * deltaY));
+        // Busca a seção das coordenadas
+        while(arquivo >> palavra){
+            if(palavra == "NODE_COORD_SECTION:" || palavra ==  "NODE_COORD_SECTION"){
+                break;
             }
         }
+
+        // Preenche os vetores das coordenadas
+        for(int i = 1; i <=  dimensao; i++){
+            arquivo >> id >> coordX[i] >> coordY[i];
+        }
+    
+        // Preenchendo a matriz com as distâncias euclidianas
+        for(int i = 1; i <= dimensao; i++){
+            for(int j = 1; j <= dimensao; j++){
+                if(i != j){
+                    double deltaX = coordX[i] - coordX[j];
+                    double deltaY = coordY[i] - coordY[j];
+                    
+                    matrizDistancias[i][j] = std::round(std::sqrt(deltaX * deltaX + deltaY * deltaY));
+                }
+            }
+        }
+    } else if(ewfTipo == "EXPLICIT"){
+        // TODO: Fazer os outros metodos de leitura
     }
+
 
     arquivo.close();
 
